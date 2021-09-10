7a06b7e29801ed5e7bee1745bf6f585dee5badcc
LayMui <laymui.toh@yara.com>
2021-09-10 11:32:59 +0800

    Add data description

--- README.md
+++ README.md
@@ -14,3 +14,25 @@ git clone https://github.com/pact-foundation/pact-broker-docker
     set PACT_BROKER_DATABASE_URL to postgres://pact_broker:password@localhost/mydb
     
 ```
+
+How does my API looks like?
+here is the curl command
+```
+curl -X 'GET' \
+  'https://address-service.laymui.io/v1/address/locations/abc’ \
+  -H 'accept: application/json' \
+  -H 'x-api-key: 1234-3456-6655
+  ```
+
+  and the response payload:
+  ```
+{"status":"SUCCESS",
+"errors":{},
+"data":
+{"locationId”:”1234-45666-333,”myUserId”:”123342-23232,”name":"My garden",
+"type":"sunflower",
+"size":"0.1",
+"unit":"acre",
+"latitude":13.741655670568623,"longitude":100.56231563692762,"country":"Singapore",
+"dependent":null,
+"address":"blk 40000 farrer road 12345",              
---
+++ __tests__/address.spec.js
@@ -0,0 +1,79 @@
+const { Pact } = require('@pact-foundation/pact');
+const { like } = require('@pact-foundation/pact').Matchers;
+const { pactWith } = require('jest-pact');
+const axios = require('axios');
+const uuid = require("uuid");
+const faker = require('faker'); //not used
+const dotenv = require("dotenv").config();
+
+
+pactWith(
+  {
+    consumer: 'iConsumer',
+    provider: 'iProvider',
+  },
+  (provider) => {
+      describe('get location api', () => {
+        const DATA = {
+         locationId: 'ca2bd511-1234b',
+         myUserId: uuid.v4(),
+         name: 'my garden',
+         type: 'SUNFLOWER',
+         size: 0.1,
+         unit: ['acre', 'hectare'],
+         latitude: -51.470544,
+         longitude: -2.588658,
+         country: 'Singapore',
+         dependent: 'Malaysia',
+         address: 'Blk 4000, farrer park, singapore'
+        };
+        const HEADER = {
+          'Content-Type': 'application/json',
+          'x-api-key': like(process.env.X_API_KEY)
+        };
+  
+        const urlPath = '/v1/address/locations' + process.env.LOCATIONID
+  
+        beforeEach(() => {
+       
+          const interaction = {
+            state: 'get address details',
+            uponReceiving: 'response.status returns locationID',
+            withRequest: {
+              method: 'GET',
+              path: urlPath,
+              headers: HEADER,
+            },
+            willRespondWith: {
+              status: 200,
+              headers: HEADER,
+              body: DATA,
+            },
+          };
+          return provider.addInteraction(interaction);
+        });
+  
+        it('returns a successful with status=SUCCESS', () => {
+          return axios
+            .request({
+              method: 'GET',
+              baseURL: provider.mockService.baseUrl,
+              url: urlPath,
+              headers: HEADER,
+            })
+            .then((response) => {
+              expect(response.data).toEqual(DATA);
+              provider
+                .verify()
+                .then((response) => {
+                  console.log(response);
+                })
+                .catch((error) => console.log(error));
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        });
+      });
+    },
+  );
